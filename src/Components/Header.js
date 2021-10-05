14d411bd66d27744024bbbc26939a1560eebd41d
alyxandria meadows <alyxmeadows@yahoo.com>
2021-10-05 10:48:24 -0500

    added fetch

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,18 @@
 
 
 
+
+
+
+
 function Header() {
+
+    fetch("http://localhost:3000/desserts")
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+
+
     return "Title and logo"
 }
 
